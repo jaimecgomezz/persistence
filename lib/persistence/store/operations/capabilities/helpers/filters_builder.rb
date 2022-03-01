cef96dc2f1ad8b4774400f4bf1f8cffcad68b679
jaimecgomezz <jaimecgomezwork@gmail.com>
2022-03-01 10:14:54 -0600

    refactor: Improve FiltersBuilder helper

--- lib/persistence/store/operations/capabilities/helpers/filters_builder.rb
+++ lib/persistence/store/operations/capabilities/helpers/filters_builder.rb
@@ -5,41 +5,110 @@ module Persistence
     module Operations
       module Capabilities
         module Helpers
+          # Transforms user-defined plain filters into descriptive filters that
+          # can be lather understood by Drivers.
+          #
+          # Example
+          #
+          # builder = FiltersBuilder.new
+          #
+          # builder.build({ id: 1 })
+          # => { __operand: :eq, __value: 1 }
+          #
+          # builder.build({ id: [1,2,3] })
+          # => { __operand: :in, __value: [1,2,3] }
+          #
+          # builder.build({ name: /[a-z]+/ })
+          # => { __operand: :like, __value: /[a-z]+/ }
+          #
+          # builder.build({ __operand: :lt, __value: 10 })
+          # { __operand: :lt, __value: 10 }
+          #
+          # builder.build({ meta: { org: { id: 1 } } })
+          # {
+          #   __operand: :nested,
+          #   __value: {
+          #     meta: {
+          #       __operand: :nested,
+          #       __value: {
+          #         org: {
+          #           __operand: :nested,
+          #           __value: {
+          #             id: {
+          #               __operand: :eq,
+          #               __value: 1
+          #             }
+          #           }
+          #         }
+          #       }
+          #     }
+          #   }
+          # }
           class FiltersBuilder
-            attr_reader :filters
+            OPERANDS = {
+              in: 'Value contained in values list',
+              between: 'Value in range between value A and B',
+              like: 'Value matching pattern',
+              nested: 'Hash value',
+              eq: 'Value should be equal',
+              not: 'Value should be different',
+              lt: 'Value should be smaller than',
+              lte: 'Value must be smaller or equal than',
+              gt: 'Value must be greater than',
+              gte: 'Value must be greater or equal than'
+            }.freeze
 
-            def initialize(filters)
-              @filters = filters
-            end
+            def build(filters)
+              if filters.is_a?(Hash)
+                return filters if is_custom_filter?(filters)
 
-            def build
-              build_hash_config(filters)
+                build_filters_from_hash(filters)
+              else
+                msg = "Filters argument must be hash, given: #{filters}"
+                raise(Persistence::Errors::OperationError, msg)
+              end
             end
 
             private
 
-            def build_hash_config(hash)
+            def build_filters_from_hash(hash)
               hash.each_with_object({}) do |pair, acc|
                 key, value = pair
-                acc[key] = filter_config_from(value)
+
+                acc[key] = build_filter_from_value(value)
               end
             end
 
-            def filter_config_from(value)
+            def build_filter_from_value(value)
               case value
               when Array
-                { operand: :in, value: value }
+                { __operand: :in, __value: value }
               when Range
-                { operand: :between, value: [value.first, value.last] }
+                { __operand: :between, __value: [value.first, value.last] }
               when Regexp
-                { operand: :like, value: value }
+                { __operand: :like, __value: value }
               when Hash
-                return value if (value.keys & %i[operand value]).size == 2
+                return value if is_custom_filter?(value)
 
-                { operand: :nested, value: build_hash_config(value) }
+                { __operand: :nested, __value: build_filters_from_hash(value) }
               else
-                { operand: :eq, value: value }
+                { __operand: :eq, __value: value }
               end
+            end
+
+            def is_custom_filter?(filter)
+              operand = filter[:__operand]
+
+              return false unless operand
+
+              return true if valid_operands.include?(operand)
+
+              msg = "Invalid operand provided to filter: #{filter}. Valid operands are: #{OPERANDS}"
+              raise(Persistence::Errors::OperationError, msg)
+            end
+
+            def valid_operands
+              @valid_operands ||= OPERANDS.keys
             end
           end
         end
--- spec/persistence/store/operations/capabilities/helpers/filters_builder_spec.rb
+++ spec/persistence/store/operations/capabilities/helpers/filters_builder_spec.rb
@@ -1,75 +1,93 @@
 # frozen_string_literal: true
 
 RSpec.describe Persistence::Store::Operations::Capabilities::Helpers::FiltersBuilder do
+  let(:builder) { described_class.new }
   let(:now) { Time.now }
   let(:tomorrow) { now + (60 * 60 * 24) }
   let(:day_after_tomorrow) { tomorrow + (60 * 60 * 24) }
-  let(:filters) do
-    {
-      a: [1, 2, 3],
-      b: now..tomorrow,
-      c: /[a-z]+/,
-      d: 'word',
-      e: {
-        operand: :lt,
-        value: 10
-      },
-      f: {
-        g: {
-          h: 10
-        }
-      }
-    }
-  end
-  let(:builder) { described_class.new(filters) }
 
   describe '.new' do
-    it 'expects filters' do
-      expect(described_class).to respond_to(:new).with(1).argument
+    it 'expects no arguments' do
+      expect(described_class).to respond_to(:new).with(0).argument
     end
   end
 
   describe '#build' do
-    let(:result) { builder.build }
-
-    it 'handles array filters' do
-      expect(result[:a]).to eq({ operand: :in, value: filters[:a] })
-    end
-
-    it 'handles range filters' do
-      range = filters[:b]
-      expect(result[:b]).to eq({ operand: :between, value: [range.first, range.last] })
-    end
-
-    it 'handles regex filters' do
-      expect(result[:c]).to eq({ operand: :like, value: filters[:c] })
-    end
-
-    it 'handles simple filters' do
-      expect(result[:d]).to eq({ operand: :eq, value: filters[:d] })
-    end
-
-    it 'handles custom filters' do
-      expect(result[:e]).to eq(filters[:e])
-    end
-
-    it 'handles nested filters' do
-      expect(result[:f]).to match(
+    context 'with valid filters' do
+      let(:filters) do
         {
-          operand: :nested,
-          value: {
+          a: [1, 2, 3],
+          b: now..tomorrow,
+          c: /[a-z]+/,
+          d: 'word',
+          e: {
+            __operand: :lt,
+            __value: 10
+          },
+          f: {
             g: {
-              operand: :nested,
-              value: {
-                h: {
-                  operand: :eq,
-                  value: 10
+              h: 10
+            }
+          }
+        }
+      end
+      let(:result) { builder.build(filters) }
+
+      it 'handles array filters' do
+        expect(result[:a]).to match({ __operand: :in, __value: filters[:a] })
+      end
+
+      it 'handles range filters' do
+        range = filters[:b]
+        expect(result[:b]).to match({ __operand: :between, __value: [range.first, range.last] })
+      end
+
+      it 'handles regex filters' do
+        expect(result[:c]).to match({ __operand: :like, __value: filters[:c] })
+      end
+
+      it 'handles simple filters' do
+        expect(result[:d]).to match({ __operand: :eq, __value: filters[:d] })
+      end
+
+      it 'handles custom filters' do
+        expect(result[:e]).to match(filters[:e])
+      end
+
+      it 'handles nested filters' do
+        expect(result[:f]).to match(
+          {
+            __operand: :nested,
+            __value: {
+              g: {
+                __operand: :nested,
+                __value: {
+                  h: {
+                    __operand: :eq,
+                    __value: 10
+                  }
                 }
               }
             }
           }
-        }
-      )
+        )
+      end
+    end
+  end
+
+  context 'with invalid filters' do
+    let(:filters) { ['invalid', 1, ['a'], Class.new].sample }
+
+    it 'raises error' do
+      expect { builder.build(filters) }.to raise_error(Persistence::Errors::OperationError)
+    end
+  end
+
+  context 'with invalid custom filters' do
+    let(:filters) { { __operand: :whatever, __value: 1 } }
+
+    it 'raises error' do
+      expect { builder.build(filters) }.to raise_error(Persistence::Errors::OperationError)
     end
   end
 end
