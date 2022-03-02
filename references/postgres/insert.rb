ee8095a0a3be8f8b65fd13c041ebd3a98d9a75c4
jaimecgomezz <jaimecgomezwork@gmail.com>
2022-03-01 20:06:08 -0600

    fix: Remove references

--- references/postgres/insert.rb
+++
@@ -1,6 +0,0 @@
-engine.insert
-  .require(operations)  # Hash[sym, SelectOperation]
-  .fields(fields)       # sym || [sym]
-  .with(map)            # Hash[sym, *]
-    .defaults
-  ,return(fields)       # sym || [sym] || Hash[sym, nil || sym || Hash[sym, sym]]
--- references/postgres/update.sql
+++
@@ -1,7 +0,0 @@
-[ WITH [ RECURSIVE ] with_query [, ...] ]
-UPDATE [ ONLY ] table [ * ] [ [ AS ] alias ]
-    SET { column = { expression | DEFAULT } |
-          ( column [, ...] ) = ( { expression | DEFAULT } [, ...] ) } [, ...]
-    [ FROM from_list ]
-    [ WHERE condition | WHERE CURRENT OF cursor_name ]
-    [ RETURNING * | output_expression [ [ AS ] output_name ] [, ...] ]
