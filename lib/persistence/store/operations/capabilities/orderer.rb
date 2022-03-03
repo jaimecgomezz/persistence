dcce83ef51b2ac86fadf246221411ae93f1acb7b
jaimecgomezz <jaimecgomezwork@gmail.com>
2022-03-03 16:10:17 -0600

    feat: Add Orderer functionality

--- lib/persistence/store/operations/capabilities/orderer.rb
+++ lib/persistence/store/operations/capabilities/orderer.rb
@@ -6,7 +6,79 @@ module Persistence
       module Capabilities
         # Makes the Operation capable of ordering results.
         module Orderer
-          def order(order)
+          # Defines an instance variable called @orderings that maps each
+          # ordering criteria to a descriptive hash that should be later
+          # understood by the Driver in order to build an ordering directive.
+          #
+          # # Handles a list of ordering criteria as positional arguments
+          # Select.all.order(:created_at, nulls: :last)
+          # => #<Select:0x000055837e273178
+          #       @orderings={
+          #         created_at: { order: :asc, nulls: :last }
+          #       }
+          #     >
+          #
+          # # Handles a list of ordering criteria as keyword arguments
+          # Select.all.order(created_at: :desc, nulls: :last)
+          # => #<Select:0x000055837e273178
+          #       @orderings={
+          #         created_at: { order: :desc, nulls: nil }
+          #       }
+          #     >
+          #
+          # # Handles a list of ordering criteria with custom mappings as keyword arguments
+          # Select.all.order(created_at: { order: :desc, nulls: :first }, nulls: :last)
+          # => #<Select:0x000055837e273178
+          #       @orderings={
+          #         created_at: { order: :desc, nulls: :first }
+          #       }
+          #     >
+          def order(*items, **kwitems)
+            nulls, remaining = kwarg_from_kwitems(kwitems, :nulls)
+
+            items.map do |item|
+              case item
+              when Symbol, String
+                field = item
+                handle_field(nulls, field)
+              else
+                invalid_orderings!
+              end
+            end
+
+            remaining.each do |field, value|
+              case value
+              when Symbol, String
+                handle_field(nulls, field, value)
+              when Hash
+                orderings[field] = value
+              else
+                invalid_orderings!
+              end
+            end
+
+            self
+          end
+
+          def orderings
+            @orderings ||= {}
+          end
+
+          private
+
+          def kwarg_from_kwitems(kwitems, kwarg)
+            value = kwitems[kwarg]
+            remaining = kwitems.slice(*kwitems.keys - [kwarg])
+            [value, remaining]
+          end
+
+          def handle_field(nulls, field, order = :asc)
+            orderings[field] = { order: order, nulls: nulls }
+          end
+
+          def invalid_orderings!
+            msg = "Invalid orderings provided to #order"
+            raise(Persistence::Errors::OperationError, msg)
           end
         end
       end
--- spec/persistence/store/operations/capabilities/orderer_spec.rb
+++ spec/persistence/store/operations/capabilities/orderer_spec.rb
@@ -1,4 +1,101 @@
 # frozen_string_literal: true
 
 RSpec.describe Persistence::Store::Operations::Capabilities::Orderer do
+  let(:mocker) { Class.new { include Persistence::Store::Operations::Capabilities::Orderer }.new }
+
+  describe '#order' do
+    context 'with postional argumets' do
+      context 'being symbols/strings' do
+        let(:resulting) { mocker.order(:created_at) }
+
+        it 'assumes a list of ordering criteria with default ordering was given' do
+          expect(resulting.orderings).to match({
+            created_at: {
+              order: :asc,
+              nulls: nil
+            }
+          })
+        end
+
+        context 'with :nulls provided' do
+          let(:resulting) { mocker.order(:created_at, nulls: :last) }
+
+          it 'includes it in each criteria mapping' do
+            expect(resulting.orderings).to match({
+              created_at: {
+                order: :asc,
+                nulls: :last
+              }
+            })
+          end
+        end
+      end
+
+      context 'being any other data type' do
+        it 'raises exception' do
+          expect { mocker.order(1) }.to raise_error(Persistence::Errors::OperationError)
+        end
+      end
+    end
+
+    context 'with keyword arguments' do
+      context 'having symbols/strings as values' do
+        let(:resulting) { mocker.order(created_at: :desc) }
+
+        it 'assumes a list of criteria with custom ordering was given' do
+          expect(resulting.orderings).to match({
+            created_at: {
+              order: :desc,
+              nulls: nil
+            }
+          })
+        end
+
+        context 'with :nulls provided' do
+          let(:resulting) { mocker.order(created_at: :desc, nulls: :first) }
+
+          it 'includes it in each criteria mapping' do
+            expect(resulting.orderings).to match({
+              created_at: {
+                order: :desc,
+                nulls: :first
+              }
+            })
+          end
+        end
+      end
+
+      context 'with hashes as values' do
+        let(:created_at_mapping) { { order: :desc, nulls: :first } }
+        let(:updated_at_mapping) { { order: :asc, nulls: :last } }
+        let(:resulting) { mocker.order(created_at: created_at_mapping, updated_at: updated_at_mapping) }
+
+        it 'assumes a list of criteria with custom mappings was given' do
+          expect(resulting.orderings).to match({
+            created_at: created_at_mapping,
+            updated_at: updated_at_mapping
+          })
+        end
+
+        context 'with :nulls provided' do
+          let(:resulting) do
+            mocker.order(created_at: created_at_mapping, updated_at: updated_at_mapping, nulls: :first)
+          end
+
+          it 'respects custom mappings' do
+            expect(resulting.orderings).to match({
+              created_at: created_at_mapping,
+              updated_at: updated_at_mapping
+            })
+          end
+        end
+      end
+
+      context 'with any other data type as values' do
+        it 'raises exception' do
+          expect { mocker.order(created_at: 1) }.to raise_error(Persistence::Errors::OperationError)
+        end
+      end
+    end
+  end
 end
