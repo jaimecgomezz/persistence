1dcccd3eb9dd8a537c8f7236a106d1c8fd129d40
jaimecgomezz <jaimecgomezwork@gmail.com>
2022-03-03 12:44:44 -0600

    refactor: Improve Aggregator with kwargs support

--- lib/persistence/store/operations/capabilities/aggregator.rb
+++ lib/persistence/store/operations/capabilities/aggregator.rb
@@ -11,15 +11,7 @@ module Persistence
           # later understood by the Driver in order to build query's
           # aggregation.
           #
-          # # Handles single field
-          # Select.where({ id: 1 }).aggregate(:income, aggregation: :sum)
-          # => #<Select:0x000055837e273178
-          #       @aggregations={
-          #         income: { alias: nil, aggregation: :sum }
-          #       }
-          #     >
-          #
-          # # Handles a list of fields
+          # # Handles a list of fields as positional arguments
           # Select.where({ id: 1 }).aggregate(:income, :age, aggregation: :sum)
           # => #<Select:0x000055837e273178
           #       @aggregations={
@@ -28,27 +20,54 @@ module Persistence
           #       }
           #     >
           #
-          # # Handles fields with alias
-          # Select.where({ id: 1 }).aggregate(:income, [:age, :AGE], aggregation: :sum)
+          # # Handles a list of fields with their aliases as keyword arguments
+          # Select.where({ id: 1 }).aggregate(:income, age: :AGE, aggregation: :sum)
           # => #<Select:0x000055837e273178
           #       @aggregations={
           #         income: { alias: nil, aggregation: :sum },
           #         age: { alias: :AGE, aggregation: :sum }
           #       }
           #     >
-          def aggregate(*items, aggregation:)
+          #
+          # # Handles a list of fields with custom mappings as keyword arguments
+          # Select.where({ id: 1 }).aggregate(
+          #   :income,
+          #   age: {
+          #     alias: :AGE,
+          #     aggregation: :max
+          #   },
+          #   aggregation: :sum
+          # )
+          # => #<Select:0x000055837e273178
+          #       @aggregations={
+          #         income: { alias: nil, aggregation: :sum },
+          #         age: { alias: :AGE, aggregation: :max }
+          #       }
+          #     >
+          def aggregate(*items, **kwitems)
+            aggregation, remaining = kwarg_from_kwitems(kwitems, :aggregation)
+
             items.map do |item|
               case item
               when Symbol, String
                 field = item
                 handle_field(aggregation, field)
-              when Array
-                field, aka = item
-                handle_field(aggregation, field, aka)
               else
                 invalid_fields!
               end
             end
+
+            remaining.each do |field, value|
+              case value
+              when Symbol, String
+                handle_field(aggregation, field, value)
+              when Hash
+                aggregations[field] = value
+              else
+                invalid_fields!
+              end
+            end
+
             self
           end
 
@@ -58,12 +77,24 @@ module Persistence
 
           private
 
+          def kwarg_from_kwitems(kwitems, kwarg)
+            value = kwitems[kwarg]
+            remaining = kwitems.slice(*kwitems.keys - [kwarg])
+            [value, remaining]
+          end
+
           def handle_field(aggregation, field, aka = nil)
+            missing_aggregation! unless aggregation
             aggregations[field] = { alias: aka, aggregation: aggregation }
           end
 
           def invalid_fields!
             msg = "Invalid fields provided to #aggregate"
+            raise(Persistence::Errors::OperationError, msg)
+          end
+
+          def missing_aggregation!
+            msg = "The kwarg :aggregation wasn't provided to #aggregate"
             raise(Persistence::Errors::OperationError, msg)
           end
         end
--- spec/persistence/store/operations/capabilities/aggregator_spec.rb
+++ spec/persistence/store/operations/capabilities/aggregator_spec.rb
@@ -8,54 +8,58 @@ RSpec.describe Persistence::Store::Operations::Capabilities::Aggregator do
       expect(mocker.aggregate(:income, aggregation: :sum)).to be(mocker)
     end
 
-    context 'with positional arguments all being symbols' do
-      let(:resulting) { mocker.aggregate(:income, :age, aggregation: :sum) }
+    context 'with positional arguments' do
+      context 'being symbols/strings' do
+        let(:resulting) { mocker.aggregate(:income, :age, aggregation: :sum) }
 
-      it 'assumes list of fields was provided' do
-        expect(resulting.aggregations).to match({
-          income: {
-            alias: nil,
-            aggregation: :sum
-          },
-          age: {
-            alias: nil,
-            aggregation: :sum
-          }
-        })
+        it 'assumes list of fields was provided' do
+          expect(resulting.aggregations).to match({
+            income: {
+              alias: nil,
+              aggregation: :sum
+            },
+            age: {
+              alias: nil,
+              aggregation: :sum
+            }
+          })
+        end
+      end
+
+      context 'not being symbols/strings' do
+        it 'raises exception' do
+          expect { mocker.aggregate(1, aggregation: :sum) }.to raise_error(Persistence::Errors::OperationError)
+        end
       end
     end
 
-    context 'with positional arguments all being lists of symbols' do
-      let(:resulting) { mocker.aggregate([:income, :INCOME], [:age, :AGE], aggregation: :sum) }
+    context 'with keyword arguments' do
+      context 'being symbols/strings' do
+        let(:resulting) { mocker.aggregate(a: :A, aggregation: :sum) }
 
-      it 'assumes list of fields with their alias was provided' do
-        expect(resulting.aggregations).to match({
-          income: {
-            alias: :INCOME,
-            aggregation: :sum
-          },
-          age: {
-            alias: :AGE,
-            aggregation: :sum
-          }
-        })
+        it "assumes field's alias was provided" do
+          expect(resulting.aggregations).to match({
+            a: {
+              alias: :A,
+              aggregation: :sum
+            }
+          })
+        end
       end
-    end
 
-    context 'with positional arguments either symbols or lists of symbols' do
-      let(:resulting) { mocker.aggregate(:income, [:age, :AGE], aggregation: :sum) }
+      context 'being hashes' do
+        let(:mapping) { { alias: :A, aggregation: :max } }
+        let(:resulting) { mocker.aggregate(a: mapping, aggregation: :sum) }
 
-      it 'assumes list of fields with optional aliases was provided' do
-        expect(resulting.aggregations).to match({
-          income: {
-            alias: nil,
-            aggregation: :sum
-          },
-          age: {
-            alias: :AGE,
-            aggregation: :sum
-          }
-        })
+        it 'assumes custom field mapping was provided' do
+          expect(resulting.aggregations).to match({ a: mapping })
+        end
+      end
+
+      context 'being any other data type' do
+        it 'raises exception' do
+          expect { mocker.aggregate(a: 1, aggregation: :sum) }.to raise_error(Persistence::Errors::OperationError)
+        end
       end
     end
   end
