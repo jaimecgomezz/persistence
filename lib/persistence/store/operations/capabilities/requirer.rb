36dfb4e031b719e3d3222120830d8f44ebdec462
jaimecgomezz <jaimecgomezwork@gmail.com>
2022-03-04 16:48:44 -0600

    docs: Add Requirer docs

--- lib/persistence/store/operations/capabilities/requirer.rb
+++ lib/persistence/store/operations/capabilities/requirer.rb
@@ -7,15 +7,40 @@ module Persistence
         # Makes the Operation being capable of requiring extra resources,
         # making them available at execution.
         module Requirer
+          # Creates the @requirements instance variable which maps aliases with
+          # resources, the latter being another Operation or plain strings if
+          # the are difficult to express with Persistence's DSL. The Driver
+          # should later understand this mappings and inject the in order to
+          # build the requiring component of the Directive.
+          #
+          # # Handles alias-Operations pairs as keyword arguments
+          # Select.requiring(
+          #   independents: Select.resource(:independent)
+          # ).resource(:dependent)
+          # => #<Select
+          #       @requirements={
+          #         independents: Select.resource(:independent)
+          #       }
+          #     >
+          #
+          # # Handles alias-Directive pairs as keyword arguments
+          # Select.requiring(
+          #   independents: "SELECT * FROM independent"
+          # ).resource(:dependent)
+          # => #<Select
+          #       @requirements={
+          #         independents: "SELECT * FROM independent"
+          #       }
+          #     >
           def requiring(**kwitems)
-            clear_previous_configuration
+            clear_requirer_configuration
 
             kwitems.each do |resource, value|
               case value
               when Persistence::Store::Operations::Operation, String
                 requirements[resource] = value
               else
-                invalid_resource!
+                invalid_required_resource!(resource)
               end
             end
 
@@ -28,12 +53,12 @@ module Persistence
 
           private
 
-          def clear_previous_configuration
+          def clear_requirer_configuration
             @requirements = {}
           end
 
-          def invalid_resource!
-            msg = "Invalid resource provided to #requiring"
+          def invalid_required_resource!(sample)
+            msg = "Invalid resource provided to #requiring: #{sample}"
             raise(Persistence::Errors::OperationError, msg)
           end
         end
