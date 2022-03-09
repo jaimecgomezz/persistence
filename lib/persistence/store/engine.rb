30c145e400d32de83ac6aff8d386f47e90c28042
jaimecgomezz <jaimecgomezwork@gmail.com>
2022-03-09 10:06:13 -0600

    feat: Implement Engine's perform method

--- lib/persistence/store/engine.rb
+++ lib/persistence/store/engine.rb
@@ -5,15 +5,71 @@ module Persistence
     # Class that acts as the proxy between the consumer (e.g. Repository) and
     # the Operation to be performed.
     class Engine
-      attr_reader :source, :driver
+      OPERATIONS = {
+        count: Operations::Count,
+        delete: Operations::Delete,
+        insert: Operations::Insert,
+        update: Operations::Update,
+        discard: Operations::Discard,
+        selection: Operations::Select
+      }
 
-      def initialize(source, driver:)
+      attr_reader :source, :driver, :operation
+
+      def initialize(source, driver)
         @source = source
         @driver = driver
       end
 
+      def perform
+        validate_operation_selected!
+        driver.run(operation)
+      rescue NoMethodError
+        raise_engine_error! "Invalid driver, it should implement #run(operation)"
+      end
+
       def perform_custom(*args, **kwargs, &block)
         driver.run_custom(*args, **kwargs, &block)
+      rescue NoMethodError
+        raise_engine_error! "Invalid driver, it should implement #run_custom"
+      end
+
+      private
+
+      def respond_to_missing?(name, pub)
+        operation.respond_to?(name) || OPERATIONS[name.to_sym] || super
+      end
+
+      def method_missing(method, *args, **kwargs, &block)
+        operation.send(method, *args, **kwargs, &block)
+        self
+      rescue NoMethodError => e
+        case e.receiver
+        when NilClass
+          operation_not_supported!(method) unless (o = OPERATIONS[method.to_sym])
+          @operation = o.new(source)
+          self
+        when operation.class
+          method_missing_in_operation!(method)
+        else
+          super
+        end
+      end
+
+      def validate_operation_selected!
+        raise_engine_error! "Can't #perform when no operation has been selected" unless operation
+      end
+
+      def operation_not_supported!(name)
+        raise_engine_error! "The operation #{name} isn't supported"
+      end
+
+      def method_missing_in_operation!(name)
+        raise_engine_error! "The #{operation.name} operation doesn't implements ##{name}"
+      end
+
+      def raise_engine_error!(msg)
+        raise(Persistence::Errors::EngineError, msg)
       end
     end
   end
--- spec/persistence/store/engine_spec.rb
+++ spec/persistence/store/engine_spec.rb
@@ -1,11 +1,75 @@
 # frozen_string_literal: true
 
 RSpec.describe Persistence::Store::Engine do
+  let(:fetch) { [] }
   let(:db) { Sequel.mock(fetch: fetch) }
   let(:driver) { Persistence::Store::Drivers::Sequel::Postgres.new(db: db) }
-  let(:mocker) { described_class.new(:users, driver: driver) }
+  let(:mocker) { described_class.new(:users, driver) }
+
+  it 'is capable of initializing a supported operation' do
+    initializer = Persistence::Store::Engine::OPERATIONS.keys.shuffle.first
+    operation = Persistence::Store::Engine::OPERATIONS[initializer]
+
+    expect(mocker.send(initializer)).to be(mocker)
+    expect(mocker.operation).to be_a(operation)
+  end
+
+  it 'is capable of proxying methods to initialized operation' do
+    expect(mocker.selection.where({ id: 1 })).to be(mocker)
+  end
+
+  it 'raises exception when trying to #perform without an initialized operation' do
+    expect do
+      mocker.perform
+    end.to raise_error(Persistence::Errors::EngineError, "Can't #perform when no operation has been selected")
+  end
+
+  it 'raises exception when proxying method not supported by the initialized operation' do
+    expect do
+      mocker.selection.whatever
+    end.to raise_error(Persistence::Errors::EngineError, "The select operation doesn't implements #whatever")
+  end
+
+  it 'raises exception when initializing an unsupported operation' do
+    expect do
+      mocker.whatever
+    end.to raise_error(Persistence::Errors::EngineError, "The operation whatever isn't supported")
+  end
+
+  describe '#perform' do
+    context 'with successful execution' do
+      let(:fetch) { [{ id: 1 }] }
+      let(:result) { mocker.selection.where({ id: 1 }).limit(1).perform }
+
+      it 'returns expected result' do
+        expect(result).to match(fetch.first)
+      end
+    end
+
+    context 'with exception during execution' do
+      let(:fetch) { StandardError }
+
+      it 'handles exception' do
+        expect do
+          mocker.selection.where({ id: 'ERROR' }).limit(1).perform
+        end .to raise_error(Persistence::Errors::DriverError, 'StandardError: StandardError')
+      end
+    end
+
+    context 'with invalid driver' do
+      let(:driver) { Class.new.new }
+
+      it 'raises exception' do
+        expect do
+          mocker.selection.perform
+        end.to raise_error(Persistence::Errors::EngineError, "Invalid driver, it should implement #run(operation)")
+      end
+    end
+  end
 
   describe '#perform_custom' do
+    let(:db) { Sequel.mock(fetch: fetch) }
+
     context 'with successful execution' do
       let(:id) { uuid }
       let(:fetch) { { id: id } }
@@ -22,8 +86,18 @@ RSpec.describe Persistence::Store::Engine do
 
       it 'handles exception' do
         expect do
-          mocker.perform_custom("SELECT * FROM users WHERE id = lola", {})
+          mocker.perform_custom("SELECT * FROM users WHERE id = :id", { id: 1 })
         end .to raise_error(Persistence::Errors::DriverError)
+      end
+    end
+
+    context 'with invalid driver' do
+      let(:driver) { Class.new.new }
+
+      it 'raises exception' do
+        expect do
+          mocker.perform_custom("", {})
+        end.to raise_error(Persistence::Errors::EngineError, "Invalid driver, it should implement #run_custom")
       end
     end
   end
