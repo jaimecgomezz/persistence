7f0f8bfe310afc6dda55b0e8f4586338fe2de9fb
jaimecgomezz <jaimecgomezwork@gmail.com>
2022-03-09 08:32:53 -0600

    feat: Add Postgres' #build method

--- lib/persistence/store/drivers/sequel/postgres.rb
+++ lib/persistence/store/drivers/sequel/postgres.rb
@@ -13,18 +13,99 @@ module Persistence
             @db = db
           end
 
+          def build(operation)
+            send(operation.name, operation)
+          rescue NoMethodError => e
+            handle_no_method_error!(e.name)
+          end
+
           def run_custom(statement, params)
             db.fetch(statement, params).all
           rescue NoMethodError
-            invalid_db!
+            handle_no_method_error!(e.name)
           rescue => e
             raise(Persistence::Errors::DriverError, e.message)
           end
 
           private
 
-          def invalid_db!
-            msg = "Invalid :db provided to #{self.class.name}, should implement #fetch(sql, params)"
+          def select(operation)
+            build_statement(
+              operation,
+              [
+                Clauses::With,
+                Clauses::Select,
+                Clauses::From,
+                Clauses::Where,
+                Clauses::Group,
+                Clauses::Order,
+                Clauses::Limit,
+                Clauses::Offset
+              ]
+            )
+          end
+
+          def delete(operation)
+            build_statement(
+              operation,
+              [
+                Clauses::Delete,
+                Clauses::Where,
+                Clauses::Returning
+              ]
+            )
+          end
+
+          def update(operation)
+            build_statement(
+              operation,
+              [
+                Clauses::Update,
+                Clauses::Set,
+                Clauses::Where,
+                Clauses::Returning
+              ]
+            )
+          end
+
+          def insert(operation)
+            build_statement(
+              operation,
+              [
+                Clauses::Insert,
+                Clauses::Values,
+                Clauses::Returning
+              ]
+            )
+          end
+
+          def count(operation)
+            build_statement(
+              operation,
+              [
+                Clauses::Select,
+                Clauses::From,
+                Clauses::Where
+              ]
+            )
+          end
+
+          alias_method :discard, :update
+
+          def build_statement(operation, clauses)
+            Builder.new(operation, clauses).build
+          end
+
+          def handle_no_method_error!(name)
+            msg = case name
+                  when 'fetch'
+                    "Invalid :db provided, it should implement #fetch(sql, params)"
+                  when 'name', 'after'
+                    "Invalid Operation provided, should implement ##{name}"
+                  else
+                    "The ##{name} operation isn't supported by the driver"
+                  end
+
             raise(Persistence::Errors::DriverError, msg)
           end
         end
--- spec/persistence/store/drivers/sequel/postgres_spec.rb
+++ spec/persistence/store/drivers/sequel/postgres_spec.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.describe Persistence::Store::Drivers::Sequel::Postgres do
-  let(:db) { Sequel.mock(fetch: fetch) }
   let(:mocker) { described_class.new(db: db) }
 
   describe '#run_custom' do
     let(:id) { uuid }
+    let(:db) { Sequel.mock(fetch: { id: id }) }
 
     context 'with successful execution' do
-      let(:fetch) { { id: id } }
       let(:result) { mocker.run_custom("SELECT * WHERE id = :id", { id: id }) }
 
       it 'runs custom statement' do
@@ -23,6 +22,92 @@ RSpec.describe Persistence::Store::Drivers::Sequel::Postgres do
         expect do
           mocker.run_custom("SELECT * WHERE id = :id", { id: id })
         end .to raise_error(Persistence::Errors::DriverError)
+      end
+    end
+  end
+
+  describe '#build' do
+    let(:db) { Class.new.new }
+    let(:result) { mocker.build(operation) }
+
+    context 'with Count operation' do
+      let(:operation) { Persistence::Store::Operations::Count.new(:a).include_discarded }
+
+      it 'build statement' do
+        params = {}
+        statement = "SELECT COUNT(a.*) FROM a"
+
+        expect(result).to match([statement, params])
+      end
+    end
+
+    context 'with Delete operation' do
+      let(:operation) { Persistence::Store::Operations::Delete.new(:a).include_discarded }
+
+      it 'build statement' do
+        params = {}
+        statement = "DELETE FROM ONLY a RETURNING *"
+
+        expect(result).to match([statement, params])
+      end
+    end
+
+    context 'with Discard operation' do
+      let(:operation) { Persistence::Store::Operations::Discard.new(:a) }
+
+      it 'build statement' do
+        discarded_attribute = ["set_", Persistence::Config::DISCARD_ATTRIBUTE].join
+        statement = "UPDATE ONLY a SET deleted_at = :#{discarded_attribute} RETURNING *"
+
+        expect(result[0]).to match(statement)
+        expect(result[1].keys).to include(discarded_attribute.to_sym)
+      end
+    end
+
+    context 'with Insert operation' do
+      let(:operation) { Persistence::Store::Operations::Insert.new(:a).values(id: 1) }
+
+      it 'build statement' do
+        params = { values_id: 1 }
+        statement = "INSERT INTO a (id) VALUES (:values_id) RETURNING *"
+
+        expect(result).to match([statement, params])
+      end
+    end
+
+    context 'with Select operation' do
+      let(:operation) { Persistence::Store::Operations::Select.new(:a).include_discarded }
+
+      it 'build statement' do
+        params = {}
+        statement = "SELECT * FROM a"
+
+        expect(result).to match([statement, params])
+      end
+    end
+
+    context 'with Update operation' do
+      let(:operation) { Persistence::Store::Operations::Update.new(:a).set(id: 2).where({ id: 1 }).include_discarded }
+
+      it 'build statement' do
+        params = { where_a_id: 1, set_id: 2 }
+        statement = "UPDATE ONLY a SET id = :set_id WHERE ((id = :where_a_id)) RETURNING *"
+
+        expect(result).to match([statement, params])
+      end
+    end
+
+    context 'with unsupported operation' do
+      let(:operation) do
+        Class.new do
+          def name
+            :undefined
+          end
+        end.new
+      end
+
+      it 'raises exception' do
+        expect { mocker.build(operation) }.to raise_error(Persistence::Errors::DriverError)
       end
     end
   end
