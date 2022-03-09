e1ce6320b7df9f19c201d52c3af7e4887089311b
jaimecgomezz <jaimecgomezwork@gmail.com>
2022-03-09 11:18:24 -0600

    feat: Add Repository specs

--- lib/persistence/repository.rb
+++ lib/persistence/repository.rb
@@ -4,7 +4,7 @@ module Persistence
   # Abstract layer that hides implementation details related to the persistence
   # layer. It has 2 main responsibilities:
   #   - Querying the DB through the Engine
-  #   - Transform Engine's result into the expected format using the Tranformer
+  #   - Transform Engine's result into the expected format using the Transformer
   #
   # Example
   #
@@ -16,9 +16,9 @@ module Persistence
   class Repository
     attr_reader :engine, :transformer
 
-    def initialize(engine:, transformer: nil)
+    def initialize(engine, transformer = nil)
       @engine = engine
-      @transformer = transformer || Transformers::UnitTransformer
+      @transformer = transformer || Transformers::UnitTransformer.new
     end
 
     def count
@@ -30,39 +30,39 @@ module Persistence
     end
 
     def all
-      transformer.many(engine.select.perform)
+      transformer.many(engine.selection.perform)
     end
 
     def first
-      transformer.one(engine.select.limit(1).perform)
+      transformer.one(engine.selection.limit(1).perform)
     end
 
     def last
-      transformer.one(engine.select.order(:desc).limit(1).perform)
+      transformer.one(engine.selection.order(:desc).limit(1).perform)
     end
 
     def find(id)
-      transformer.one(engine.select.where({ id: id }).limit(1).perform)
+      transformer.one(engine.selection.where({ id: id }).limit(1).perform)
     end
 
-    def find_many(ids)
-      transformer.many(engine.select.where({ id: ids }).perform)
+    def find_many(*ids)
+      transformer.many(engine.selection.where({ id: ids }).perform)
     end
 
     def find_discarded(id)
-      transformer.one(engine.select.include_discarded.where({ id: id }).limit(1).perform)
+      transformer.one(engine.selection.include_discarded.where({ id: id }).limit(1).perform)
     end
 
-    def find_many_discarded(ids)
-      transformer.many(engine.select.include_discarded.where({ id: ids }).perform)
+    def find_many_discarded(*ids)
+      transformer.many(engine.selection.include_discarded.where({ id: ids }).perform)
     end
 
     def find_where(filters)
-      transformer.one(engine.select.where(filters).limit(1).perform)
+      transformer.one(engine.selection.where(filters).limit(1).perform)
     end
 
     def find_many_where(filters)
-      transformer.many(engine.select.where(filters).perform)
+      transformer.many(engine.selection.where(filters).perform)
     end
 
     def create(input)
@@ -76,7 +76,7 @@ module Persistence
     end
 
     def update(id, input)
-      tranformer.one(engine.update.where({ id: id }).set(input).perform)
+      transformer.one(engine.update.where({ id: id }).set(input).perform)
     end
 
     def update_where(filters, input)
@@ -87,6 +87,10 @@ module Persistence
       transformer.one(engine.delete.include_discarded.where({ id: id }).perform)
     end
 
+    def destroy_many(*ids)
+      transformer.many(engine.delete.include_discarded.where({ id: ids }).perform)
+    end
+
     def destroy_where(filters)
       transformer.many(engine.delete.include_discarded.where(filters).perform)
     end
@@ -95,8 +99,12 @@ module Persistence
       transformer.one(engine.discard.where({ id: id }).perform)
     end
 
+    def discard_many(*ids)
+      transformer.many(engine.discard.where({ id: ids }).perform)
+    end
+
     def discard_where(filters)
-      tranformer.many(engine.discard.where(filters).perform)
+      transformer.many(engine.discard.where(filters).perform)
     end
   end
 end
--- spec/persistence/store/repository_spec.rb
+++ spec/persistence/store/repository_spec.rb
@@ -1,4 +1,214 @@
 # frozen_string_literal: true
 
 RSpec.describe Persistence::Repository do
+  let(:engine) do
+    Persistence::Store::Engine.new(
+      :users,
+      Persistence::Store::Drivers::Sequel::Postgres.new(db: Sequel.mock(fetch: fetch))
+    )
+  end
+
+  let(:repository) { described_class.new(engine) }
+
+  describe '.new' do
+    let(:fetch) { [] }
+
+    it 'expects engine and driver' do
+      expect(described_class).to respond_to(:new).with(2).arguments
+    end
+
+    it 'uses the unit transformer by default' do
+      expect(repository.transformer).to be_a(Persistence::Transformers::UnitTransformer)
+    end
+  end
+
+  describe '#count' do
+    let(:fetch) { [{ count: 10 }] }
+    let(:result) { repository.count }
+
+    it 'returns count' do
+      expect(result).to be(10)
+    end
+  end
+
+  describe '#count_where' do
+    let(:fetch) { [{ count: 1 }] }
+    let(:result) { repository.count_where({ id: 1 }) }
+
+    it 'returns count' do
+      expect(result).to be(1)
+    end
+  end
+
+  describe '#all' do
+    let(:fetch) { [{ id: 1 }, { id: 2 }] }
+    let(:result) { repository.all }
+
+    it 'returns all records' do
+      expect(result).to match(fetch)
+    end
+  end
+
+  describe '#first' do
+    let(:fetch) { [{ id: 1 }] }
+    let(:result) { repository.first }
+
+    it 'returns first record' do
+      expect(result).to match(fetch.first)
+    end
+  end
+
+  describe '#last' do
+    let(:fetch) { [{ id: 2 }] }
+    let(:result) { repository.last }
+
+    it 'returns last record' do
+      expect(result).to match(fetch.last)
+    end
+  end
+
+  describe '#find' do
+    let(:fetch) { [{ id: 1 }] }
+    let(:result) { repository.find(1) }
+
+    it 'returns record with id' do
+      expect(result).to match(fetch.first)
+    end
+  end
+
+  describe '#find_many' do
+    let(:fetch) { [{ id: 1 }, { id: 2 }] }
+    let(:result) { repository.find_many(1, 2) }
+
+    it 'returns records with ids' do
+      expect(result).to match(fetch)
+    end
+  end
+
+  describe '#find_discarded' do
+    let(:fetch) { [{ id: 1 }] }
+    let(:result) { repository.find_discarded(1) }
+
+    it 'returns discarded record with id' do
+      expect(result).to match(fetch.first)
+    end
+  end
+
+  describe '#find_many_discarded' do
+    let(:fetch) { [{ id: 1 }, { id: 2 }] }
+    let(:result) { repository.find_many_discarded(1, 2) }
+
+    it 'returns discarded records with ids' do
+      expect(result).to match(fetch)
+    end
+  end
+
+  describe '#find_where' do
+    let(:fetch) { [{ id: 1, org: 1 }, { id: 2, org: 1 }] }
+    let(:result) { repository.find_where({ org: 1 }) }
+
+    it 'returns first matching record' do
+      expect(result).to match(fetch.first)
+    end
+  end
+
+  describe '#find_many_where' do
+    let(:fetch) { [{ id: 1, org: 1 }, { id: 2, org: 1 }] }
+    let(:result) { repository.find_many_where({ org: 1 }) }
+
+    it 'returns matching records' do
+      expect(result).to match(fetch)
+    end
+  end
+
+  describe '#create' do
+    let(:fetch) { [{ id: 1 }] }
+    let(:result) { repository.create({ id: 1 }) }
+
+    it 'returns record created' do
+      expect(result).to match(fetch.first)
+    end
+  end
+
+  describe '#bulk' do
+    let(:fetch) { [[{ id: 1 }], [{ id: 2 }]] }
+    let(:result) { repository.bulk([{ id: 1 }, { id: 2 }]) }
+
+    it 'returns created records' do
+      expect(result).to match([{ id: 1 }, { id: 2 }])
+    end
+  end
+
+  describe '#update' do
+    let(:fetch) { [{ id: 2 }] }
+    let(:result) { repository.update(1, { id: 2 }) }
+
+    it 'returns updated record' do
+      expect(result).to match(fetch.first)
+    end
+  end
+
+  describe '#update_where' do
+    let(:fetch) { [{ id: 1, name: 'updated' }] }
+    let(:result) { repository.update_where({ id: 1 }, { name: 'updated' }) }
+
+    it 'returns record updated' do
+      expect(result).to match(fetch.first)
+    end
+  end
+
+  describe '#destroy' do
+    let(:fetch) { [{ id: 1 }] }
+    let(:result) { repository.destroy(1) }
+
+    it 'returns destroyed records' do
+      expect(result).to match(fetch.first)
+    end
+  end
+
+  describe '#destroy_many' do
+    let(:fetch) { [{ id: 1 }, { id: 2 }] }
+    let(:result) { repository.destroy_many(1, 2) }
+
+    it 'returns destroyed records' do
+      expect(result).to match(fetch)
+    end
+  end
+
+  describe '#destroy_where' do
+    let(:fetch) { [{ id: 1, name: 'Luis' }, { id: 2, name: 'Luis' }] }
+    let(:result) { repository.destroy_where({ name: 'Luis' }) }
+
+    it 'returns destroyed records' do
+      expect(result).to match(fetch)
+    end
+  end
+
+  describe '#discard' do
+    let(:fetch) { [{ id: 1, deleted_at: Time.now.utc.to_s }] }
+    let(:result) { repository.discard(1) }
+
+    it 'returns discarded record' do
+      expect(result).to match(fetch.first)
+    end
+  end
+
+  describe '#discard_many' do
+    let(:fetch) { [{ id: 1, deleted_at: Time.now.utc.to_s }, { id: 2, deleted_at: Time.now.utc.to_s }] }
+    let(:result) { repository.discard_many(1, 2) }
+
+    it 'returns discarded records' do
+      expect(result).to match(fetch)
+    end
+  end
+
+  describe '#discard_where' do
+    let(:now) { Time.now.utc.to_s }
+    let(:fetch) { [{ id: 1, name: 'Luis', deleted_at: now }, { id: 2, name: 'Luis', deleted_at: now }] }
+    let(:result) { repository.discard_where({ name: 'Luis' }) }
+
+    it 'returns' do
+      expect(result).to match(fetch)
+    end
+  end
 end
